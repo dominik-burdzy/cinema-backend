de2453e1331cca798b94ee6c947f4df21379a537
Dominik Burdzy <d.burdzy@dental21.de>
2024-12-28 21:19:15 +0100

    tech: Cleanup unused configs

--- src/main/kotlin/pl/dombur/cinema/Application.kt
+++ src/main/kotlin/pl/dombur/cinema/Application.kt
@@ -1,29 +1,9 @@
 package pl.dombur.cinema
 
-import org.springframework.boot.actuate.autoconfigure.audit.AuditAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
-import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration
-import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.retry.annotation.EnableRetry
-import org.springframework.scheduling.annotation.EnableAsync
-import org.springframework.scheduling.annotation.EnableScheduling
 
-@EnableAsync
-@EnableRetry
-@EnableScheduling
-@SpringBootApplication(
-    scanBasePackages = ["pl.dombur.cinema.**"],
-    exclude = [
-        AuditAutoConfiguration::class,
-        SessionAutoConfiguration::class,
-        RedisAutoConfiguration::class,
-        RedisReactiveAutoConfiguration::class,
-        RedisRepositoriesAutoConfiguration::class,
-    ],
-)
+@SpringBootApplication(scanBasePackages = ["pl.dombur.cinema.**"])
 class Application
 
 fun main(args: Array<String>) {
